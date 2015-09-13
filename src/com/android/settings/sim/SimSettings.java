2a07174f9ef97c9b363495ab72246b7a37e31682
Sandeep Gutta <sangutta@codeaurora.org>
2015-09-13 22:09:23 +0530

    Fix to do not allow both SIM deactivation
    Use proper SubInfo record object to do not allow
    user to deactivate both SIMs on DSDS devices.
    
    Change-Id: I6f56d819367272b3a009c7a8133709c643e8bd84
    CRs-Fixed: 893504

--- src/com/android/settings/sim/SimSettings.java
+++ src/com/android/settings/sim/SimSettings.java
@@ -598,7 +598,7 @@ public class SimSettings extends RestrictedSettingsFragment implements Indexable
                     mSubscriptionManager.getActiveSubscriptionInfoList();
             if (subInfoLists != null) {
                 for (SubscriptionInfo subInfo : subInfoLists) {
-                    if (getProvisionStatus(mSir.getSimSlotIndex())
+                    if (getProvisionStatus(subInfo.getSimSlotIndex())
                             == PROVISIONED) activeSubInfoCount++;
                 }
             }

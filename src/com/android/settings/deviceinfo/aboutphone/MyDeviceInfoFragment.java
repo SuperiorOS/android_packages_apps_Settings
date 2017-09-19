dd69c9b0a663d18f07c13ce928973cbfaf562c7c
ezio84 <brabus84@gmail.com>
2017-09-18 21:23:40 -0400

    Remove feedback and bug report stuff
    Change-Id: I37da83c64ba148262d74d49d32910499d60b6692
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
+++ src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -129,7 +129,7 @@ public class MyDeviceInfoFragment extends DashboardFragment
         controllers.add(new RegulatoryInfoPreferenceController(context));
         controllers.add(new SafetyInfoPreferenceController(context));
         controllers.add(new ManualPreferenceController(context));
-        controllers.add(new FeedbackPreferenceController(fragment, context));
+        //controllers.add(new FeedbackPreferenceController(fragment, context));
         controllers.add(new FccEquipmentIdPreferenceController(context));
         controllers.add(
                 new BuildNumberPreferenceController(context, activity, fragment, lifecycle));

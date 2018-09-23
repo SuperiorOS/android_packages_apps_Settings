c7a4c4b8345fbd1f65fa9283553be1fa893e9762
blinoff82 <blinov.in@gmail.com>
2018-09-23 17:06:40 +0300

    Settings: completely remove unneeded collapse limits *We don't need a minimized settings.

--- src/com/android/settings/password/ChooseLockPatternSize.java
+++ src/com/android/settings/password/ChooseLockPatternSize.java
@@ -143,7 +143,7 @@ public class ChooseLockPatternSize extends PreferenceActivity {
 
         @Override
         public int getMetricsCategory() {
-            return MetricsEvent.XTENDED;
+            return MetricsEvent.SUPERIOR;
         }
     }
 }

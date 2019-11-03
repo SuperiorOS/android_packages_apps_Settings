803d55b42b0db4c9dcd8689d2e0e8cc68615bc63
Jayant Deshmukh <jayantdeshmuk008@gmail.com>
2019-11-03 06:46:16 +0000

    Revert "Disable dashboards conditions and suggestions (2/2)"
    This reverts commit 7a3d3d0a441c07de032e94289247642846f9ed9d.

--- src/com/android/settings/homepage/contextualcards/conditional/ConditionManager.java
+++ src/com/android/settings/homepage/contextualcards/conditional/ConditionManager.java
@@ -17,7 +17,6 @@
 package com.android.settings.homepage.contextualcards.conditional;
 
 import android.content.Context;
-import android.provider.Settings;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -45,14 +44,12 @@ public class ConditionManager {
 
     private final ExecutorService mExecutorService;
     private final Context mAppContext;
-    private static Context mContext;
     private final ConditionListener mListener;
 
     private boolean mIsListeningToStateChange;
 
     public ConditionManager(Context context, ConditionListener listener) {
         mAppContext = context.getApplicationContext();
-        mContext = context;
         mExecutorService = Executors.newCachedThreadPool();
         mCardControllers = new ArrayList<>();
         mListener = listener;
@@ -181,9 +178,7 @@ public class ConditionManager {
 
         @Override
         public ContextualCard call() throws Exception {
-            return (mController.isDisplayable()
-                    && (Settings.System.getInt(mContext.getContentResolver(),
-                       Settings.System.ENABLE_CONDITIONS, 1) == 1)) ? mController.buildContextualCard() : null;
+            return mController.isDisplayable() ? mController.buildContextualCard() : null;
         }
     }
 }

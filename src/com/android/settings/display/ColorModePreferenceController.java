bd0fe0d05874de58d5b901b811614b99560f5393
Marco Marinho <marcomarinho12@gmail.com>
2020-09-09 01:18:21 +0000

    Settings: fixup! Check if we have any color modes declared in overlay
    * Checking for wide gamut support isn't always enough
    
    [marcomarinho]: let's switch to OR condition since we want Colors to appear either way, not if all conditions match. This will make it appear on devices with the overlay enabled
    **[test1]: add colorModes overlay and check if "Colors" is under Settings>Display
    **[test2]: remove colorModes overlay and check it "Colors" is not present under Settings>Display
    ** all tests are passed so we got a working and right behaviour.
    
    Change-Id: I60fe0440504ec3966eb274e20262e32eac48c989

--- src/com/android/settings/display/ColorModePreferenceController.java
+++ src/com/android/settings/display/ColorModePreferenceController.java
@@ -33,7 +33,7 @@ public class ColorModePreferenceController extends BasePreferenceController {
     public int getAvailabilityStatus() {
         final int[] availableColorModes = mContext.getResources().getIntArray(
                 com.android.internal.R.array.config_availableColorModes);
-        return availableColorModes.length > 0 &&
+        return availableColorModes.length > 0 ||
                 mContext.getSystemService(ColorDisplayManager.class).isDeviceColorManaged()
                 && !ColorDisplayManager.areAccessibilityTransformsEnabled(mContext) ?
                 AVAILABLE_UNSEARCHABLE : DISABLED_FOR_USER;

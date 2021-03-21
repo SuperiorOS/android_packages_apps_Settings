0de216f7c86d463d50e8246b289c4a8ac8251b69
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2021-03-21 15:16:30 +0100

    [SQUASH]Revert Face unlock stuffs [2/4]
    Revert "Settings: Swipe up on keyguard to use Face Unlock [2/2]"
    
    This reverts commit 5a1599a51dfc615833698d1303ebd1bc871b6ad5.
    
    Revert "Settings: Port face unlock feature [2/3]"
    
    This reverts commit 6ae7abf3aea2aa65f92079d0a360f6209d0a5025.
    
    Change-Id: I8fd307fd1c9fc019d04259d4571a05901b22d53b
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- src/com/android/settings/biometrics/face/FaceSettings.java
+++ src/com/android/settings/biometrics/face/FaceSettings.java
@@ -71,7 +71,6 @@ public class FaceSettings extends DashboardFragment {
     private FaceSettingsRemoveButtonPreferenceController mRemoveController;
     private FaceSettingsEnrollButtonPreferenceController mEnrollController;
     private FaceSettingsLockscreenBypassPreferenceController mLockscreenController;
-    private FaceSettingsSwipePreferenceController mSwipeController;
     private List<AbstractPreferenceController> mControllers;
 
     private List<Preference> mTogglePreferences;
@@ -163,7 +162,6 @@ public class FaceSettings extends DashboardFragment {
         Preference confirmPref = findPreference(FaceSettingsConfirmPreferenceController.KEY);
         Preference bypassPref =
                 findPreference(mLockscreenController.getPreferenceKey());
-        Preference swipePref = findPreference(mSwipeController.getPreferenceKey());
         mTogglePreferences = new ArrayList<>(
                 Arrays.asList(keyguardPref, appPref, attentionPref, confirmPref, bypassPref));
 
@@ -184,7 +182,6 @@ public class FaceSettings extends DashboardFragment {
         if (mUserManager.isManagedProfile(mUserId)) {
             removePreference(FaceSettingsKeyguardPreferenceController.KEY);
             removePreference(mLockscreenController.getPreferenceKey());
-            removePreference(mSwipeController.getPreferenceKey());
         }
 
         if (savedInstanceState != null) {
@@ -198,8 +195,6 @@ public class FaceSettings extends DashboardFragment {
 
         mLockscreenController = use(FaceSettingsLockscreenBypassPreferenceController.class);
         mLockscreenController.setUserId(mUserId);
-        mSwipeController = use(FaceSettingsSwipePreferenceController.class);
-        mSwipeController.setUserId(mUserId);
     }
 
     @Override

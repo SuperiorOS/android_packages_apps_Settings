c7cc1bb704448498ecddcbd7b94237bd357315b9
Joker-V2 <nipinna0@gmail.com>
2021-07-12 09:47:54 +0530

    Settings: Drop some redundant stuffs from themes
    - In favour of style picker
    
    Change-Id: I3c44f99eaa408807d949097299f3ac02340d5606

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -133,12 +133,6 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new SwitchStylePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
         controllers.add(mFontPickerPreference = new FontPickerPreferenceController(context, lifecycle));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.accent_color"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.font"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.adaptive_icon_shape"));
         return controllers;
     }
 

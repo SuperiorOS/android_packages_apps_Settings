a051b3084d2c186bda806aa922939cf7875bf5bc
Joker-V2 <nipinna0@gmail.com>
2021-09-05 14:25:33 +0000

    Revert "dashboard: Implement Spring animation effect"
    - Not smooth as expected and glitchy af
    This reverts commit 55944763a579889302a5471ba8b6f876b23cd9a2.
    
    Change-Id: I8ed017b3cce64afe5b94451ac7a8fc50cfbd106f

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -26,7 +26,6 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.settings.R;
 import com.android.settings.core.SubSettingLauncher;
@@ -35,8 +34,6 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.support.SupportPreferenceController;
 import com.android.settingslib.core.instrumentation.Instrumentable;
 import com.android.settingslib.search.SearchIndexable;
-
-import static android.view.View.OVER_SCROLL_NEVER;
 
 @SearchIndexable(forTarget = MOBILE)
 public class TopLevelSettings extends DashboardFragment implements
@@ -54,13 +51,6 @@ public class TopLevelSettings extends DashboardFragment implements
     @Override
     protected int getPreferenceScreenResId() {
         return R.xml.top_level_settings;
-    }
-
-     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        RecyclerView recyclerView = getView().findViewById(R.id.recycler_view);
-        recyclerView.setOverScrollMode(OVER_SCROLL_NEVER);
     }
 
     @Override

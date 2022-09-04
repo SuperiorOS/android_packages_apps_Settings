ffd1a27ed1d73f9b1305c97f58c965da31a3a991
maxwen <max.weninger@gmail.com>
2022-09-04 12:34:44 +0530

    Settings: Add SuperiorLab to dashboard
    Change-Id: Ie18a29e62a651b4b5c74510493dc291135f5361c

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -436,4 +436,7 @@ public class Settings extends SettingsActivity {
      * Activity for OneHandedSettings
      */
     public static class OneHandedSettingsActivity extends SettingsActivity { /* empty */ }
+
+    // SuperiorLab Additoon
+    public static class SuperiorLabActivity extends SettingsActivity {}
 }

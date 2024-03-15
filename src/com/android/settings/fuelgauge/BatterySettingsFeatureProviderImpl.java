56a0ad7239c8b29dac0b0c51e9c750469ab5622f
Michael Bestas <mkbestas@gmail.com>
2024-03-15 16:10:49 +0200

    [SQUASH] Make battery info and cycle count configurable
    - Also enable them by default.
    - This is a squash of the following commits:
    https://github.com/LineageOS/android_packages_apps_Settings/commit/781740000ba0aaa80b6a8478adfd9188c38ec6be
    https://github.com/LineageOS/android_packages_apps_Settings/commit/e1cea87cc610720fa62b564506b908f873a4024e
    
    Change-Id: If38db229e420e9f7c0ac26abb42a2a309b2f87c2
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- src/com/android/settings/fuelgauge/BatterySettingsFeatureProviderImpl.java
+++ src/com/android/settings/fuelgauge/BatterySettingsFeatureProviderImpl.java
@@ -18,6 +18,7 @@ package com.android.settings.fuelgauge;
 
 import android.content.Context;
 
+import com.android.settings.R;
 import com.android.settings.fuelgauge.batterytip.BatteryTipPolicy;
 import com.android.settings.fuelgauge.batterytip.detectors.LowBatteryDetector;
 import com.android.settings.fuelgauge.batterytip.tips.BatteryTip;
@@ -39,7 +40,7 @@ public class BatterySettingsFeatureProviderImpl implements BatterySettingsFeatur
 
     @Override
     public boolean isBatteryInfoEnabled(Context context) {
-        return false;
+        return context.getResources().getBoolean(R.bool.config_show_battery_info);
     }
 
     @Override
